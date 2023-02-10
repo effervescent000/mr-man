5b170faea0e720be34f35786f1cfb4db49da8f7c
effervescent000 <kislio01@gmail.com>
2023-02-10 15:55:00 -0500

    attempting to unfuck everything

--- app/components/nav/header.tsx
+++ app/components/nav/header.tsx
@@ -1,12 +1,16 @@
 import { routeMap } from "~/routeMap";
+import AccountStatus from "./account-status";
 import NavItem from "./nav-item";
 
 const Header = () => {
   return (
-    <div>
-      {routeMap.map(({ label, path }, index) => (
-        <NavItem key={index} label={label} path={path} />
-      ))}
+    <div className="flex justify-between">
+      <div>
+        {routeMap.map(({ label, path }, index) => (
+          <NavItem key={index} label={label} path={path} />
+        ))}
+      </div>
+      <AccountStatus />
     </div>
   );
 };
